687d4c89eb7b9b9550cf8867289c8e88cbaea275
medicsera <anton.mirko.0550@gmail.com>
2025-09-08 22:51:25 +0700

    done 5

--- lab_1/task_5.py
+++ lab_1/task_5.py
@@ -1,31 +1,22 @@
-# Проведите эксперимент, сравнивающий производительность оператора del для словарей и
+# Проведите эксперимент, сравнивающий производительность оператора in для множеств и
 # списков. Покажите их поведение при помощи графика зависимости времени от количества входных данных.
-# 
 
 from time import time
-import numpy as np
 import matplotlib.pyplot as plt
 
-# Словари
-def del_dict(n):
-    d = {i: i for i in range(n)}
-    for i in range(n):
-        del d[i]  
+# Использует хэщ-таблицу, что помогает быстрее находить элемент в списке
+def in_set(n):
+    s = set(range(n))  
+    _ = -1 in s  
     return 
 
 
-# Удаление одного элемента это O(1), соответственно O(n) - удаление всего словаря
-# Т.к. удаление происходит по ключу и внутренних смещений не происходит
-
-# Списки
-def del_list(n):
-    lst = list(range(n))
-    for i in range(n):
-        del lst[0]
+# Перебирает весь список, вследствие чего поиск замедляется
+def in_list(n):
+    lst = list(range(n)) 
+    _ = -1 in lst  
     return 
 
-# Здесь же одно удаление занимает O(n), что означает что весь список будет занимать O(n)*n или же O(n**2)
-# Так происходит из-за того, что все оставшиеся элементы списка смещаются после удаления одного из элементов.
 
 def measure_algorithm_time(func, n):
     tic = time()
@@ -42,8 +33,8 @@ def create_performance_chart():
     print("Измеряем время выполнения для разных размеров")
     
     for size in input_sizes:
-        time_one = measure_algorithm_time(del_dict, size)
-        time_two = measure_algorithm_time(del_list, size)
+        time_one = measure_algorithm_time(in_set, size)
+        time_two = measure_algorithm_time(in_list, size)
 
         times_func_one.append(time_one)
         times_func_two.append(time_two)
